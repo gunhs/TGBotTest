e3c05ef5cdfad4e27ec61151ae84c9fd1c75ce46
Gunhs <mr-vaska@ya.ru>
2023-02-11 23:06:14 +0300

    add Events

---
+++ src/main/java/ru/sharanov/SearchForMessagesBot/Storage/DBEvents.java
@@ -0,0 +1,32 @@
+package ru.sharanov.SearchForMessagesBot.Storage;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DBEvents {
+    private List<String> events;
+    private static final String PATH = "data/events.txt";
+
+    public DBEvents() throws IOException {
+        events = new ArrayList<>();
+        String[] s = readFile(Path.of(PATH)).split("\n");
+        events = List.of(s);
+    }
+
+    private String readFile(Path path) throws IOException {
+        List<String> strings = Files.readAllLines(path);
+        StringBuilder result = new StringBuilder();
+        strings.forEach(s->result.append(s).append("\n"));
+        return result.toString();
+    }
+
+    public void addEvents(String event){
+        events.add(event);
+    }
+    public ArrayList<String> getEvents(){
+        return new ArrayList<>(events);
+    }
+}
--- src/main/java/ru/sharanov/SearchForMessagesBot/service/TelegramBot.java
+++ src/main/java/ru/sharanov/SearchForMessagesBot/service/TelegramBot.java
@@ -10,8 +10,12 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.chatmember.ChatMember;
 import org.telegram.telegrambots.meta.api.objects.chatmember.ChatMemberMember;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import ru.sharanov.SearchForMessagesBot.Storage.DBEvents;
 import ru.sharanov.SearchForMessagesBot.config.BotConfig;
 import org.telegram.api.messages.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
@@ -34,40 +38,53 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        if (update.hasMessage() && update.getMessage().hasText()) {
-            String messageText = update.getMessage().getText();
-            long chatId = update.getMessage().getChatId();
-            System.out.println(messageText);
-            switch (messageText) {
-                case "/start" -> startCommandReceived(chatId, update.getMessage().getFrom().getFirstName());
+        try {
+            if (update.hasMessage() && update.getMessage().hasText()) {
+                String messageText = update.getMessage().getText();
+                long chatId = update.getMessage().getChatId();
+                System.out.println(messageText);
+                switch (messageText) {
+                    case "/start" -> startCommandReceived(chatId, update.getMessage().getFrom().getFirstName());
 //                case "/Katya" -> katyaCommandReceived(chatId);
 //                case "/Anya" -> anyaCommandReceived(chatId);
-                case "/Настя" -> find36(chatId);
-                default -> sendMessage(chatId, "Sorry, command was not recognized");
+//                case "/Настя" -> find36(chatId);
+                    case "/add" -> getEvents(chatId);
+                    default -> sendMessage(chatId, "Sorry, command was not recognized");
+                }
             }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
     private void find36(long chatId) {
-
-//        String answer = "Анечка, ты просто космос ✨" ;
-        String answer = "Бот, который отвечает Насте" ;
+        String answer = "Бот, который отвечает Насте";
         sendMessage(chatId, answer);
     }
 
     private void startCommandReceived(long chatId, String name) {
-        String answer = "Hi," +  name + ", nice to meet you";
+        String answer = "Hi," + name + ", nice to meet you";
         sendMessage(chatId, answer);
     }
 
-    private void katyaCommandReceived(long chatId){
+    private void katyaCommandReceived(long chatId) {
         String answer = "Эта строчка специально для Кати, чтобы она не выпендривалась";
         sendMessage(chatId, answer);
     }
 
-    private void anyaCommandReceived(long chatId){
+    private void anyaCommandReceived(long chatId) {
         String answer = "Анечка, ты просто космос ✨! ";
         sendMessage(chatId, answer);
+    }
+
+    private void getEvents(long chatId) throws IOException {
+        DBEvents dbEvents = new DBEvents();
+        ArrayList<String> events = dbEvents.getEvents();
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < events.size(); i++) {
+            answer.append(i + 1).append(". ").append(events.get(i)).append("\n");
+        }
+        sendMessage(chatId, answer.toString().trim());
     }
 
     private void sendMessage(long chatId, String textToSend) {
