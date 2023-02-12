33a3f5f062272689c834501879d25d57cb47ff05
gunhs <mr-vaska@ya.ru>
2023-02-12 18:17:48 +0500

    add help

--- src/main/java/ru/sharanov/SearchForMessagesBot/Storage/DBparticipant.java
+++ src/main/java/ru/sharanov/SearchForMessagesBot/Storage/DBparticipant.java
@@ -4,24 +4,37 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class DBparticipant {
     List<String> participants;
+    HashMap<String, String> participantsMap;
+    List<String> nickNames;
     private static final String PATH = "data/participant.txt";
+    private static final String PATHTONICKNAMES = "data/nicknames.txt";
     public DBparticipant() throws IOException {
-//        participants = new ArrayList<>();
         participants = readFile(Path.of(PATH));
-
+        nickNames = readFile(Path.of(PATHTONICKNAMES));
     }
-    public void addParticipant(String newParticipant) throws IOException {
-//        if (!participant.contains(newParticipant)){
-            participants.add(newParticipant);
-//        }
+    public String addParticipant(String newParticipant, String nickName) throws IOException {
+        if (nickNames.contains(nickName)){
+            return newParticipant;
+        }
+        nickNames.add(nickName);
+        participants.add(newParticipant + " (@" + nickName+")");
+
         StringBuilder listOfParticipant = new StringBuilder();
         participants.forEach(p-> listOfParticipant.append(p).append("\n"));
         Files.write(Path.of(PATH), listOfParticipant.toString().getBytes());
+
+        StringBuilder listOfNickNames = new StringBuilder();
+        nickNames.forEach(p-> listOfNickNames.append(p).append("\n"));
+        Files.write(Path.of(PATHTONICKNAMES), listOfNickNames.toString().getBytes());
+        return "";
     }
+
+
 
     public ArrayList<String> getParticipants() {
         return new ArrayList<>(participants);
--- src/main/java/ru/sharanov/SearchForMessagesBot/service/TelegramBot.java
+++ src/main/java/ru/sharanov/SearchForMessagesBot/service/TelegramBot.java
@@ -40,12 +40,11 @@ public class TelegramBot extends TelegramLongPollingBot {
                 System.out.println(messageText);
                 switch (messageText) {
                     case "/start" -> startCommandReceived(chatId, update.getMessage().getFrom().getFirstName());
-//                case "/Katya" -> katyaCommandReceived(chatId);
-//                case "/Anya" -> anyaCommandReceived(chatId);
-//                case "/Настя" -> find36(chatId);
                     case "/Мероприятия" -> getEvents(chatId);
-                    case "/Добавить меня" -> addParticipant(chatId, update.getMessage().getFrom().getFirstName());
+                    case "/Добавить меня" -> addParticipant(update.getMessage().getFrom().getFirstName(), update.getMessage().getFrom().getUserName(), chatId);
                     case "/Список" -> getParticipant(chatId);
+                    case "/Исключить меня" -> removeParticipant(chatId, update.getMessage().getFrom().getUserName());
+                    case "/help" -> showCommand(chatId);
                     default -> sendMessage(chatId, "Sorry, command was not recognized");
                 }
             }
@@ -54,24 +53,39 @@ public class TelegramBot extends TelegramLongPollingBot {
         }
     }
 
+    private void showCommand(long chatId) {
+        String answer = """
+                Список комманд:
+                /Мероприятия - показать список ближайших мероприятий
+                /Добавить меня - добавить участника на ближайшее мероприятие
+                /Список - показать список участников ближайшего мероприятия
+                /Исключить меня - исключить участника из участников ближайшего мероприятия""";
+        sendMessage(chatId, answer.trim());
+    }
+
+    private void removeParticipant(long chatId, String userName) {
+        StringBuilder answer = new StringBuilder("Список участников билжайшого мероприятия:\n");
+    }
+
     private void getParticipant(long chatId) throws IOException {
         DBparticipant dBparticipant = new DBparticipant();
         ArrayList<String> participants = dBparticipant.getParticipants();
-        StringBuilder answer = new StringBuilder();
+        StringBuilder answer = new StringBuilder("Список участников билжайшого мероприятия:\n");
         for (int i = 0; i < participants.size(); i++) {
             answer.append(i + 1).append(". ").append(participants.get(i)).append("\n");
         }
         sendMessage(chatId, answer.toString().trim());
     }
 
-    private void addParticipant(long chatId, String firstName) throws IOException {
-        DBparticipant dBparticipant= new DBparticipant();
-        dBparticipant.addParticipant(firstName);
-    }
-
-    private void find36(long chatId) {
-        String answer = "Бот, который отвечает Насте";
-        sendMessage(chatId, answer);
+    private void addParticipant(String firstName, String nickName, long chatId) throws IOException {
+        DBparticipant dBparticipant = new DBparticipant();
+        if (!dBparticipant.addParticipant(firstName, nickName).isEmpty()) {
+            String answer = "Вы уже добавлены";
+            sendMessage(chatId, answer);
+        } else {
+            String answer = "Вы успешно добавлены";
+            sendMessage(chatId, answer);
+        }
     }
 
     private void startCommandReceived(long chatId, String name) {
@@ -79,20 +93,10 @@ public class TelegramBot extends TelegramLongPollingBot {
         sendMessage(chatId, answer);
     }
 
-    private void katyaCommandReceived(long chatId) {
-        String answer = "Эта строчка специально для Кати, чтобы она не выпендривалась";
-        sendMessage(chatId, answer);
-    }
-
-    private void anyaCommandReceived(long chatId) {
-        String answer = "Анечка, ты просто космос ✨! ";
-        sendMessage(chatId, answer);
-    }
-
     private void getEvents(long chatId) throws IOException {
         DBEvents dbEvents = new DBEvents();
         ArrayList<String> events = dbEvents.getEvents();
-        StringBuilder answer = new StringBuilder();
+        StringBuilder answer = new StringBuilder("Список билжайших мероприятий:\n");
         for (int i = 0; i < events.size(); i++) {
             answer.append(i + 1).append(". ").append(events.get(i)).append("\n");
         }
