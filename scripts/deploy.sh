30b4932139a2c7a7ee11750d0d1416df0d8a4683
gunhs <mr-vaska@yandex.ru>
2023-04-22 04:45:09 +0300

    add on server

---
+++ scripts/deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+#mvn clean package
+
+ssh -i ~/.ssh/id_ed25519 root@80.90.191.154 <<EOF
+
+#pgrep java  | xargs kill -9
+
+nohup java -jar bots/TGBotTest/target/SearchForMessagesBot-0.0.1-SNAPSHOT.jar > log.txt &
+
+EOF
