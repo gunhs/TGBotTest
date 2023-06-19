bd03a29b918b5899d9ff7f4f997e8654abcc5040
Gunhs <mr-vaska@ya.ru>
2023-06-19 16:31:22 +0500

    edit script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,5 +7,5 @@ ssh -i ~/.ssh/id_ed25519 root@80.90.191.154 <<EOF
 #pgrep java  | xargs kill -9
 
 nohup java -jar bots/TGBotTest/target/SearchForMessagesBot-0.0.1-SNAPSHOT.jar > log.txt &
-
+nohup java -jar bots/TGBotTest/out/TGBotTest_jar/TGBotTest.jar
 EOF
