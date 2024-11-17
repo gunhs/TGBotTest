2f8a50e39bf86146e255c8a2502968daee975512
Gunhs <mr-vaska@ya.ru>
2024-11-17 23:00:10 +0300

    change ROLES

--- src/main/java/ru/sharanov/JavaEventTelgeramBot/security/AppAuthenticationSuccessHandler.java
+++ src/main/java/ru/sharanov/JavaEventTelgeramBot/security/AppAuthenticationSuccessHandler.java
@@ -1,16 +1,16 @@
-package ru.sharanov.JavaEventTelgeramBot.security;
-
-import org.springframework.security.core.Authentication;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-public class AppAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
-    protected void handle(HttpServletRequest request, HttpServletResponse response,
-                          Authentication authentication) throws IOException, ServletException {
-
-    }
-}
+//package ru.sharanov.JavaEventTelgeramBot.security;
+//
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+//
+//import javax.servlet.ServletException;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//
+//public class AppAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+//    protected void handle(HttpServletRequest request, HttpServletResponse response,
+//                          Authentication authentication) throws IOException, ServletException {
+//
+//    }
+//}
